69bc31e46c243c73962a1628994d8e92d014b1d8
darl1ng-design <adam.sp220@gmail.com>
2022-04-15 16:00:16 +0200

    Update signup.php

--- pages/signup.php
+++ pages/signup.php
@@ -1,29 +1,20 @@
 <?php 
-
-
-if (isset($_POST['submit'])){
   $nick=$_POST['nickname'];  
   $email=$_POST['email'];  
   $license_key=$_POST['license_key'];  
   $pwd=$_POST['pwd'];  
-
-
    require_once 'functions.php';
     require_once 'db.php';
 
    if(emptyInput($nick, $email, $license_key, $pwd) !== false){
-    header("location: ./main.php?error=emptyinput");
+    //header("location: ./main.php?error=emptyinput");
+    echo json_encode('Error: empty input');
     exit();
    }
 
-   elseif(userExists($conn, $nick, $email, $license_key, $pwd) !== false){
-    header("location: ./main.php?error=userexists");
+   elseif (userExists($conn, $nick, $email, $license_key, $pwd) !== false){
+    //header("location: ./main.php?error=userexists");
+    echo json_encode('Error: user exists');
     exit();
    }
-}
-else{
-    header("location: ./main.php");
-    exit();
-}
-
 ?>
